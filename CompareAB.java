f5cb5c8650db30cfbb23b7c4a1a4ef3540783b0f
Subhashini J <subhashinijaichandran21072003@gmail.com>
2025-02-01 16:10:03 +0530

    Add files via upload

---
+++ CompareAB.java
@@ -0,0 +1,12 @@
+class CompareAB{
+public static void main(String[] args){
+char a='A';
+char b='B';
+System.out.println("Comparing "+a+"and "+b);
+System.out.println("A<B "+(a<b));
+System.out.println("A<=B "+(a<=b));
+System.out.println("A>B "+(a>b));
+System.out.println("A>=B "+(a>=b));
+System.out.println("A==B "+(a==b));
+System.out.println("A!=B "+(a!=b));
+}}
